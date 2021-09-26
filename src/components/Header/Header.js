6918940b49772edd38b30febd26c9810414dc10c
karakib2k18 <karakib2k18@gmail.com>
2021-09-26 16:33:42 +0600

    developers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className='container bg bg-red pt-5 pb-5 header-bg'>
-            <h1>New Generation Banking System Development Team </h1>
+            <h1>New Generation Banking System Developers Team </h1>
             <h5>We have selected best developer for developing user friendly, secure online or offline banking system. In this website you can see our developers profile, their salaries,role and experience</h5>
             <h2>Total Budget: $50 Million USD</h2>
         </div>
