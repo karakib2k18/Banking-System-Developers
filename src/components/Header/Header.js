2b014aa56beb825957df31ed610f5d6df44e91fa
karakib2k18 <karakib2k18@gmail.com>
2021-09-26 15:01:52 +0600

    header-color-change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ const Header = () => {
     return (
         <div className='container bg bg-red pt-5 pb-5 header-bg'>
             <h1>New Generation Banking System Development Team </h1>
-            <h5>We have selected best developer for developing user friendly, secure a online or offline banking system. </h5>
-            <h3>Total Budget: $50 Million USD</h3>
+            <h4>We have selected best developer for developing user friendly, secure a online or offline banking system. </h4>
+            <h2>Total Budget: $50 Million USD</h2>
         </div>
     );
 };
